50c78c98c7b01660ee50c8cea1b07984cf7b2595
yeremiavictor <yeremiavictor@email.com>
2022-10-08 11:48:32 +0700

    init add function

--- instavr/app/Controllers/Post.php
+++ instavr/app/Controllers/Post.php
@@ -13,5 +13,44 @@ class Post extends BaseController
         echo view('post/list', $data);
     }
 
+    public function add()
+    {
+        $data['namaSistem'] = 'VRINSTA';
+        $validtaion = \Config\Services::validation();
+        $validation->setRules(['foto' => 'required']);
+        $isDataValid = $validation->withRequest($this->request)->run();
 
+        if($isDataValid){
+            
+            //get foto
+                $foto = $this->request->getFile('foto');
+
+            //check upload
+            if($foto->getError() == 4){
+                //nothing
+            } else {
+                //getname
+                $namafoto = $foto->getName();
+                //acak nama
+                $namafoto = $foto->getRandomName();
+                // move to img
+                $foto->move('img', $namafoto);
+            }
+
+            $Post = new PostModel();
+            $Post->insert([
+                'id'=>$this->request->getPost('id'),
+                'post'=>$this->request->getPost('post'),
+                'foto'=>$this->request->getPost('foto'),
+                'foto'=>$namafoto,
+                'comment'=>$this->request->getPost('comment'),
+                'like'=>$this->request->getPost('like'),
+            ]);
+            return redirect()->to(base_url('/Post'));
+        }
+
+        echo view('post/add', $data);
+
+
+    }
 }

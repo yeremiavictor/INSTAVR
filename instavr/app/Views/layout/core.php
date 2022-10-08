57699473b69314e906176e99cd956585c16c7c50
yeremiavictor <yeremiavictor@email.com>
2022-10-08 11:23:59 +0700

    conf interface layout

--- instavr/app/Views/layout/core.php
+++ instavr/app/Views/layout/core.php
@@ -10,6 +10,14 @@
     <!-- Page level plugins -->
     <script src=<?= base_url("vendor/chart.js/Chart.min.js") ?>></script>
 
+    <!-- Page level plugins -->
+    <script src=<?= base_url("vendor/chart.js/Chart.min.js") ?>></script>
+
     <!-- Page level custom scripts -->
-    <script src=<?= base_url("js/demo/chart-area-demo.js") ?>></script>
-    <script src=<?= base_url("js/demo/chart-pie-demo.js") ?>></script>
+    <link href=<?= base_url("vendor/datatables/dataTables.bootstrap4.min.css") ?> rel="stylesheet">
+    <!-- Page level plugins -->
+    <script src=<?= base_url("vendor/datatables/jquery.dataTables.min.js") ?> ></script>
+    <script src=<?= base_url("vendor/datatables/dataTables.bootstrap4.min.js") ?> ></script>
+
+    <!-- Page level custom scripts -->
+    <script src=<?= base_url("js/demo/datatables-demo.js") ?> ></script>
--- instavr/app/Views/layout/sidebar.php
+++ instavr/app/Views/layout/sidebar.php
@@ -16,7 +16,7 @@
             <li class="nav-item active">
                 <a class="nav-link" href="index.html">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>VRINSTA</span></a>
             </li>
 
 
@@ -29,38 +29,38 @@
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
+                <!-- <li class="nav-item">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                        aria-expanded="true" aria-controls="collapsePages">
+                        <i class="fas fa-fw fa-folder"></i>
+                        <span>Pages</span>
+                    </a>
+                    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                            <h6 class="collapse-header">Login Screens:</h6>
+                            <a class="collapse-item" href="login.html">Login</a>
+                            <a class="collapse-item" href="register.html">Register</a>
+                            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+                            <div class="collapse-divider"></div>
+                            <h6 class="collapse-header">Other Pages:</h6>
+                            <a class="collapse-item" href="404.html">404 Page</a>
+                            <a class="collapse-item" href="blank.html">Blank Page</a>
+                        </div>
                     </div>
-                </div>
-            </li>
+                </li> -->
 
             <!-- Nav Item - Charts -->
             <li class="nav-item">
-                <a class="nav-link" href="charts.html">
+                <a class="nav-link" href="#">
                     <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
+                    <span>Post</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+                <a class="nav-link" href="#">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    <span>Keluar</span></a>
             </li>
 
             <!-- Divider -->
